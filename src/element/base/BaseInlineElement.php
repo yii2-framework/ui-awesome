fff3b4f163993bfa33445efef85f9fea65dd940c
Wilmer Arambula <terabytesoftw@gmail.com>
2025-11-18 11:45:55 -0300

    refactor(elements): Remove unused imports and streamline inline element validation logic.

--- src/element/base/BaseInlineElement.php
+++ src/element/base/BaseInlineElement.php
@@ -7,8 +7,6 @@ namespace yii\ui\element\base;
 use UnitEnum;
 use yii\base\InvalidArgumentException;
 use yii\ui\content\flow\InlineContent;
-use yii\ui\content\flow\VoidContent;
-use yii\ui\element\VoidElement;
 use yii\ui\exception\Message;
 use yii\ui\helpers\{Attributes, Encode, Enum};
 
@@ -68,10 +66,6 @@ abstract class BaseInlineElement
     ): string {
         $tag = (string) Enum::normalizeValue($tag);
         $tag = strtolower($tag);
-
-        if (VoidContent::isVoid($tag)) {
-            return VoidElement::render($tag, $attributes);
-        }
 
         if (InlineContent::isInline($tag) === false) {
             throw new InvalidArgumentException(Message::INVALID_INLINE_ELEMENT->getMessage($tag));
