94132085c1c770330020b678e1d24bc2d97b4264
Wilmer Arambula <terabytesoftw@gmail.com>
2025-12-09 07:39:28 -0300

    feat(attributes):

---
+++ tests/attributes/HasDirectionTest.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace yii\ui\tests\attributes;
+
+use PHPUnit\Framework\Attributes\{DataProviderExternal, Group};
+use PHPUnit\Framework\TestCase;
+use UnitEnum;
+use yii\ui\attributes\HasDir;
+use yii\ui\helpers\Attributes;
+use yii\ui\mixin\HasAttributes;
+use yii\ui\tests\providers\tag\attributes\DirectionProvider;
+
+/**
+ * Test suite for {@see HasDir} trait functionality and behavior.
+ *
+ * Validates the management of the global HTML `dir` attribute according to the HTML Living Standard specification.
+ *
+ * Ensures correct handling, immutability, and validation of the `dir` attribute in tag rendering, supporting `string`,
+ * `UnitEnum`, and `null` values for dynamic direction assignment.
+ *
+ * Test coverage:
+ * - Accurate rendering of attributes with the `dir` attribute.
+ * - Data provider-driven validation for edge cases and expected behaviors.
+ * - Immutability of the trait's API when setting or overriding the `dir` attribute.
+ * - Proper assignment and overriding of `dir` values.
+ *
+ * {@see DirectionProvider} for test case data providers.
+ *
+ * @copyright Copyright (C) 2025 Terabytesoftw.
+ * @license https://opensource.org/license/bsd-3-clause BSD 3-Clause License.
+ */
+#[Group('attributes')]
+final class HasDirectionTest extends TestCase
+{
+    /**
+     * @phpstan-param mixed[] $attributes
+     */
+    #[DataProviderExternal(DirectionProvider::class, 'renderAttribute')]
+    public function testRenderAttributesWithDirectionAttribute(
+        string|UnitEnum|null $dir,
+        array $attributes,
+        string|UnitEnum $expected,
+        string $message,
+    ): void {
+        $instance = new class {
+            use HasAttributes;
+            use HasDir;
+        };
+
+        $instance = $instance->attributes($attributes)->dir($dir);
+
+        self::assertSame(
+            $expected,
+            Attributes::render($instance->getAttributes()),
+            $message,
+        );
+    }
+
+    public function testReturnEmptyWhenDirAttributeNotSet(): void
+    {
+        $instance =  new class {
+            use HasAttributes;
+            use HasDir;
+        };
+
+        self::assertEmpty(
+            $instance->getAttributes(),
+            'Should have no attributes set when no attribute is provided.',
+        );
+    }
+
+    public function testReturnNewInstanceWhenSettingDirAttribute(): void
+    {
+        $instance = new class {
+            use HasAttributes;
+            use HasDir;
+        };
+
+        self::assertNotSame(
+            $instance,
+            $instance->dir(''),
+            'Should return a new instance when setting the attribute, ensuring immutability.',
+        );
+    }
+
+    /**
+     * @phpstan-param mixed[] $attributes
+     */
+    #[DataProviderExternal(DirectionProvider::class, 'values')]
+    public function testSetDirAttributeValue(
+        string|UnitEnum|null $dir,
+        array $attributes,
+        string|UnitEnum $expected,
+        string $message,
+    ): void {
+        $instance = new class {
+            use HasAttributes;
+            use HasDir;
+        };
+
+        $instance = $instance->attributes($attributes)->dir($dir);
+
+        self::assertSame(
+            $expected,
+            $instance->getAttributes()['dir'] ?? '',
+            $message,
+        );
+    }
+}
